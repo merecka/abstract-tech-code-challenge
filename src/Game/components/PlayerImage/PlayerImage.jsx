903da66cb04202277d77841849fa972d829f109c
alex-merecka <amerecka@webcreek.com>
2022-06-23 12:46:57 -0500

    Solution working.  Need to re-factor backup img into variable.

--- src/Game/components/PlayerImage/PlayerImage.jsx
+++ src/Game/components/PlayerImage/PlayerImage.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
-import { css } from './PlayerImage.style';
-import { useCss } from '@abst/hooks';
-import { /* Text, */ View } from '@abst/web-components';
-import { usePlayer } from '@src/Game/hooks';
-import cn from 'classnames';
+import React from "react";
+import { css } from "./PlayerImage.style";
+import { useCss } from "@abst/hooks";
+import { Text, View } from "@abst/web-components";
+import { usePlayer } from "@src/Game/hooks";
+import cn from "classnames";
 
-const pfx = 'https://cdn.nba.com/headshots/nba/latest/1040x760';
+const pfx = "https://cdn.nba.com/headshots/nba/latest/1040x760";
 
 export function PlayerImage({
-  bg, playerId, maxHeight, shadow = true, onClick
+  bg,
+  playerId,
+  maxHeight,
+  shadow = true,
+  onClick,
 }) {
   const player = usePlayer(playerId);
   const cls = useCss(css, {
     bg,
     maxHeight,
-    teamColors: player?.team?.colors
+    teamColors: player?.team?.colors,
   });
 
   if (!_.isFinite(playerId)) return null;
 
   return (
     <View
-      className={ cn(cls.wpr, { 'no-bg': bg === false, shadow }) }
-      role='button'
-      { ...{ onClick } }
+      className={cn(cls.wpr, { "no-bg": bg === false, shadow })}
+      role="button"
+      {...{ onClick }}
     >
       {/* NOTE: uncomment this (and the import statement) to see the name of the player */}
-      {/* <Text t={ `(This is ${player.displayFirstLast})` } className='cheat' /> */}
+      <Text t={`(This is ${player.displayFirstLast})`} className="cheat" />
       <img
-        src={ `${pfx}/${playerId}.png` }
+        src={`${pfx}/${playerId}.png`}
+        onError={(e) => {
+          e.currentTarget.onerror = null;
+          e.currentTarget.src =
+            "https://cdn.nba.com/headshots/nba/latest/1040x760/logoman.png";
+        }}
+        // onError={({ currentTarget }) => {
+        //   currentTarget.onerror = null; // prevents looping
+        //   currentTarget.src={ `${pfx}/logoman.png` }
+        // }}
       />
     </View>
   );
